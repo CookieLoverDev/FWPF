5650f076c3cc0c5786becf875e2e6cc09b95af19
CookieLoverDev <ukhodjamov@gmail.com>
2024-10-28 17:58:43 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Counter___Color_Picker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int userValue;
+        public string userText {  get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            userValue = 0;
+            userText = "Current Value: ";
+            DataContext = this;
+        }
+
+        public void IncreaseButton(object sender, RoutedEventArgs e)
+        {
+            userValue++;
+            ValueText.Text = userText + userValue;
+        }
+
+        public void DecreaseButton(object sender, RoutedEventArgs e)
+        {
+            userValue--;
+            ValueText.Text = userText + userValue;
+        }
+
+        public void ChangeBlue(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Background = new SolidColorBrush(Colors.Blue);
+        }
+
+        public void ChangeGreen(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Background = new SolidColorBrush(Colors.Green);
+        }
+
+        public void ChangeYellow(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Background = new SolidColorBrush(Colors.Yellow);
+        }
+    }
+}
